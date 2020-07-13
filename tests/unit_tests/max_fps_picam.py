5c33335e94f3e776fa2624e7a2fed16260ef9637
Jeff Bass <jeffbass42@gmail.com>
2020-07-12 17:24:06 -0700

    Adjust print statements

--- tests/unit_tests/max_fps_picam.py
+++ tests/unit_tests/max_fps_picam.py
@@ -11,37 +11,40 @@ import sys
 
 import time
 import cv2
-from imutils.video import VideoStream, FPS
+from datetime import datetime
+from imutils.video import VideoStream
 
-SHOW_IMAGES = False  # SHOW_IMAGES will slow down FPS; only use to check PiCamera
+SHOW_IMAGES = True  # SHOW_IMAGES will slow down FPS; only use to check PiCamera
 NUM_FRAMES = 1000  # How many frames to capture for this test
 
 picam = VideoStream(usePiCamera=True,resolution=(320, 240),framerate=32).start()
 time.sleep(2.0)  # allow camera sensor to warm up
-fps = FPS().start()  # start FPS timer after first image is received
+start = datetime.now()
 frame_count = 0
 
 while frame_count <= NUM_FRAMES:  # send images as stream until Ctrl-C
     image = picam.read()
     frame_count += 1
-    fps.update()
     if SHOW_IMAGES:  # SHOW_IMAGES will slow down FPS
         cv2.imshow('PiCamera', image)  # 1 window for each RPi
         cv2.waitKey(1)
 
-fps.stop()
+stop = datetime.now()
 print('FPS Test Program: ', __file__)
 print('Option settings:')
 print('    Show Images?', SHOW_IMAGES)
 print('    Requested Frames?', NUM_FRAMES)
 print('    Actual Frames Read:', frame_count)
-image_size = image.shape
+image_size = image.shape  # want size expressed numpy dimentions
 print('Size of last image received: ', image_size)
 uncompressed_size = 1
 for dimension in image_size:
     uncompressed_size *= dimension
-print('    = {:,g} bytes'.format(uncompressed_size))
-print('Elasped time: {:,.2f} seconds'.format(fps.elapsed()))
-print('Approximate FPS: {:.2f}'.format(fps.fps()))
+print('    = {:,}'.format(uncompressed_size))
+elapsed = stop - start  # elapsed time datetime.timedelta() format
+seconds = elapsed.total_seconds()
+print('Elasped time:', seconds)
+fps = frame_count / seconds
+print('Approximate FPS: {:.2f}'.format(fps))
 cv2.destroyAllWindows()  # closes the windows opened by cv2.imshow()
 sys.exit()
