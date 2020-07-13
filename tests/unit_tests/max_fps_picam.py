dd8b861ce2e3041a15cc517ffa5abebbc98e47c8
Jeff Bass <jeffbass42@gmail.com>
2020-07-12 23:33:08 -0700

    Improve FPS tests.

--- tests/unit_tests/max_fps_picam.py
+++ tests/unit_tests/max_fps_picam.py
@@ -1,8 +1,8 @@
-"""max_fps_picam.py -- determine maximum FPS for different PiCamera modes
+"""max_fps_picam.py -- determine maximum FPS for different PiCamera framerates
 
-All the imagenode programs that use the PiCamera capture_continuous method.
+Framerate settings don't seem to affect PiCamera capture_continuous method.
 This test program counts how many frames per second that the camera is able
-to capture, using the imutils package.
+to capture.
 
 It computes and prints FPS statistics.
 """
@@ -14,15 +14,16 @@ import cv2
 from datetime import datetime
 from imutils.video import VideoStream
 
-SHOW_IMAGES = True  # SHOW_IMAGES will slow down FPS; only use to check PiCamera
-NUM_FRAMES = 1000  # How many frames to capture for this test
+SHOW_IMAGES = False  # SHOW_IMAGES will slow down FPS; only use to check PiCamera
+NUM_FRAMES = 10000  # How many frames to capture for this test
 
-picam = VideoStream(usePiCamera=True,resolution=(320, 240),framerate=32).start()
+picam = VideoStream(usePiCamera=False,resolution=(320, 240),framerate=1).start()
 time.sleep(2.0)  # allow camera sensor to warm up
+image = picam.read()  # read one image before loop to trap potential errors
 start = datetime.now()
 frame_count = 0
 
-while frame_count <= NUM_FRAMES:  # send images as stream until Ctrl-C
+while frame_count < NUM_FRAMES:  # send images as stream until Ctrl-C
     image = picam.read()
     frame_count += 1
     if SHOW_IMAGES:  # SHOW_IMAGES will slow down FPS
@@ -32,19 +33,18 @@ while frame_count <= NUM_FRAMES:  # send images as stream until Ctrl-C
 stop = datetime.now()
 print('FPS Test Program: ', __file__)
 print('Option settings:')
-print('    Show Images?', SHOW_IMAGES)
-print('    Requested Frames?', NUM_FRAMES)
-print('    Actual Frames Read:', frame_count)
-image_size = image.shape  # want size expressed numpy dimentions
-print('Size of last image received: ', image_size)
-uncompressed_size = 1
-for dimension in image_size:
-    uncompressed_size *= dimension
-print('    = {:,}'.format(uncompressed_size))
+print('    Show Images? {}'.format(SHOW_IMAGES))
+print('    Requested Frames: {:,}'.format(NUM_FRAMES))
+print('    Actual Frames Read:  {:,}'.format(frame_count))
+print('Dimensions of last image received: ', image.shape)
+size = 1
+for dimension in image.shape:
+    size *= dimension
+print('    = {:,} bytes'.format(size))
 elapsed = stop - start  # elapsed time datetime.timedelta() format
 seconds = elapsed.total_seconds()
-print('Elasped time:', seconds)
+print('Elasped time:', seconds, 'seconds')
 fps = frame_count / seconds
-print('Approximate FPS: {:.2f}'.format(fps))
+print('Approximate FPS: {:,.2f}'.format(fps))
 cv2.destroyAllWindows()  # closes the windows opened by cv2.imshow()
 sys.exit()
