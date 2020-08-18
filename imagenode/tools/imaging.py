dd2171351aad21499b77370b4664345d4da0febb
Jeff Bass <jeffbass42@gmail.com>
2020-08-18 16:04:20 -0700

    Add PiCameraUnthreadedStream class

--- imagenode/tools/imaging.py
+++ imagenode/tools/imaging.py
@@ -603,22 +603,22 @@ class PiCameraUnthreadedStream():
     def __init__(self, resolution=(320, 240), framerate=32, **kwargs):
         from picamera.array import PiRGBArray
         from picamera import PiCamera
-    	self.camera = PiCamera()
-    	self.camera.resolution = resolution
-    	self.camera.framerate = framerate
-    	self.rawCapture = PiRGBArray(self.camera, size=resolution)
-    	self.stream = self.camera.capture_continuous(self.rawCapture,
-    		format="bgr", use_video_port=True)
-    	self.frame = None
+        self.camera = PiCamera()
+        self.camera.resolution = resolution
+        self.camera.framerate = framerate
+        self.rawCapture = PiRGBArray(self.camera, size=resolution)
+        self.stream = self.camera.capture_continuous(self.rawCapture,
+            format="bgr", use_video_port=True)
+        self.frame = None
 
     def read(self):
         f = next(self.stream)  # or f = self.stream.read()?
         self.frame = f.array
-    	self.rawCapture.truncate(0)
-    	return self.frame
+        self.rawCapture.truncate(0)
+        return self.frame
 
     def stop(self):
-    	self.camera.close()
+        self.camera.close()
 
     def close(self):
         self.camera.close()
