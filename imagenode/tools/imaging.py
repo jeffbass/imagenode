6bc57a92d587bc1683576eb03793d7b07b5b9029
Jeff Bass <jeffbass42@gmail.com>
2020-08-18 16:01:39 -0700

    Add PiCameraUnthreadedSteam class

--- imagenode/tools/imaging.py
+++ imagenode/tools/imaging.py
@@ -600,29 +600,28 @@ class PiCameraUnthreadedStream():
     without threading, primarily intended for testing. For compatibility, the
     method names are the same as imutils.VideoStream.
     """
-	def __init__(self, resolution=(320, 240), framerate=32, **kwargs):
+    def __init__(self, resolution=(320, 240), framerate=32, **kwargs):
         from picamera.array import PiRGBArray
         from picamera import PiCamera
-		self.camera = PiCamera()
-		self.camera.resolution = resolution
-		self.camera.framerate = framerate
-		self.rawCapture = PiRGBArray(self.camera, size=resolution)
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
-		self.frame = None
+    	self.camera = PiCamera()
+    	self.camera.resolution = resolution
+    	self.camera.framerate = framerate
+    	self.rawCapture = PiRGBArray(self.camera, size=resolution)
+    	self.stream = self.camera.capture_continuous(self.rawCapture,
+    		format="bgr", use_video_port=True)
+    	self.frame = None
 
-	def read(self):
+    def read(self):
         f = next(self.stream)  # or f = self.stream.read()?
         self.frame = f.array
-		self.rawCapture.truncate(0)
-		return self.frame
+    	self.rawCapture.truncate(0)
+    	return self.frame
 
-	def stop(self):
-		self.camera.close()
+    def stop(self):
+    	self.camera.close()
 
     def close(self):
         self.camera.close()
-
 
 class Camera:
     """ Methods and attributes of a camera
